36f1c8945dc74ca088f4d9aa492055094d4d7eaf
BozidarK93 <bozidar.kokot93@gmail.com>
2021-01-16 22:55:21 +0100

    Initial Commit

---
+++ Brewery-Bloc/Brewery_BlocApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brewery_BlocApp.swift
+//  Brewery-Bloc
+//
+//  Created by Bozidar Kokot on 16.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Brewery_BlocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brewery-Bloc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Brewery-Bloc
+//
+//  Created by Bozidar Kokot on 16.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
